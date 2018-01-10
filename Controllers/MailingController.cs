06465f706b6b0aeda827a875370020ce0c32f0e6
Ron Nims <rleenims@gmail.com>
2018-01-10 12:03:57 -0800

    Initial commit of FirstLook app, Assignment 1

---
+++ Controllers/MailingController.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using FirstLook.Models;
+
+namespace FirstLook.Controllers
+{
+    public class MailingController : Controller
+    {
+        // GET: Mailing
+        public ActionResult Index()
+        {
+            Mailing m1 = new Mailing();
+            m1.FirstName = "Ron";
+            m1.LastName = "Nims";
+            m1.Email = "rln@aol.com";
+            Mailing m2 = new Mailing();
+            m2.FirstName = "Oprah";
+            m2.LastName = "Winfrey";
+            m2.Email = "oprah@aol.com";
+            Mailing m3 = new Mailing();
+            m3.FirstName = "Joe";
+            m3.LastName = "Biden";
+            m3.Email = "job@gmail.com";
+
+            List<Mailing> mailings = new List<Mailing>();
+            mailings.Add(m1);
+            mailings.Add(m2);
+            mailings.Add(m3);
+
+
+            return View(mailings);
+
+        }
+    }
+}
